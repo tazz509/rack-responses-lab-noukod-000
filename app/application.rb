68a5e673e79f54d5367f3bede1b4149f36eece8b
carly <carly@noukod.com>
2018-12-17 20:39:02 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -1,23 +1,15 @@
 class Application
- 
+
   def call(env)
     resp = Rack::Response.new
- 
-    num_1 = Kernel.rand(1..20)
-    num_2 = Kernel.rand(1..20)
-    
- 
-    resp.write "#{num_1}\n"
-    resp.write "#{num_2}\n"
-    
- 
-    if num_1==num_2 && num_2==num_3
+    time=Time.now;
+        if (time.hour > 12)
       resp.write  "Good Morning!"
     else
       resp.write  "Good Afternoon!"
     end
- 
+
     resp.finish
   end
- 
+
 end
